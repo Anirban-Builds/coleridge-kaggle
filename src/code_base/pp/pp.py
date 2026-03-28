313dd5e96a0bb2cc1784417395dcd2cc2389789f
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-03-28 22:32:19 +0530

    upd

--- src/code_base/pp/pp.py
+++ src/code_base/pp/pp.py
@@ -67,7 +67,8 @@ class Preprocess:
         for sentence in sentences:
             ispositive, tags = self._ner(sentence, labels)
             if self.inference:
-                ner_lst.append(tags)
+                if any(word in sentence for word in ['data', 'study', 'survey', 'dataset']):
+                    ner_lst.append(tags)
             else:
                 if ispositive:
                     ner_lst.append(tags)
