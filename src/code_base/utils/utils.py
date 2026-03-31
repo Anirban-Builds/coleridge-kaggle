667e6d93f5eb84c9909dd0923af4ab484e5ccfad
Anirban0011 <bhattandbhatt.999@gmail.com>
2026-03-31 08:44:20 +0530

    upd

--- src/code_base/utils/utils.py
+++ src/code_base/utils/utils.py
@@ -2,6 +2,6 @@ import re
 
 def clean_text(txt):
     '''
-    Function for cleaning text and preprocessing 
+    Function for cleaning text and preprocessing
     '''
-    return re.sub('[^A-Za-z0-9]+', ' ', str(txt).lower()).strip()
+    return re.sub('[^A-Za-z0-9]+', ' ', str(txt)).strip()
